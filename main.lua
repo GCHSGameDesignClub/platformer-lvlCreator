04ebb6c43360d1594130ffc2750528727c7b7284
jfecher <jfecher11@gmail.com>
2015-11-30 19:35:08 -0500

    Added data labels, and added rudimentary support for textboxes

--- main.lua
+++ main.lua
@@ -1,15 +1,23 @@
 
 
 function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    sb_base = love.graphics.newImage('tilesetCheckerboard.png')
+    --lvl creator specific vars
+    showData = false
+    lvlName = "unsaved level"
+    state = "lvl" --gamestates: lvl, inp
+    inpStr = ""
+    inpVar = ""
 
+    --level display setup
+    setTileset("tilesetBase.png")
+    
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setFullscreen(1, "desktop")
-    sb = love.graphics.newSpriteBatch(sb_base, 256, 'static')
 
     scrWid = love.window.getWidth()
     scrHei = love.window.getHeight()
 
+    --amount of tiles shown in each dimension
     sqByWid = 32
     sqByHei = 18
 
@@ -21,11 +29,56 @@ function love.load()
 end
 
 
+function setTileset(fileName)
+    tilesetName = fileName
+    sb_base = love.graphics.newImage(tilesetName)
+    sb = love.graphics.newSpriteBatch(sb_base, 256, 'static')
+end
+
+
+function love.keypressed(key)
+    if state == "lvl" then
+        if key == "1" then --toggle display
+            showData = not showData
+        elseif key == "n" then
+            getInput("lvlName")
+        end
+    elseif state == "inp" then
+        handleTextBoxInput(key)
+    end
+end
+
 
 function love.update()
 
 end
 
+
+function handleTextBoxInput(key)
+    if key == "escape" then --return failure
+        state = "lvl"
+    elseif key == "return" then --return success
+        state = "lvl"
+        _G[inpVar] = inpStr
+    elseif key == "backspace" then
+        inpStr = inpStr:sub(1, #inpStr-1)
+    else
+        inpStr = inpStr .. key
+    end
+end
+
+
+function getInput(var)
+    state = "inp"
+    inpStr = ""
+    inpVar = var
+end
+
+
+function max(a, b)
+    if a > b then return a
+    else return b end
+end
 
 
 function love.draw()
@@ -38,4 +91,20 @@ function love.draw()
         end
     end
     love.graphics.pop()
+
+    if state == "lvl" then
+        if showData then
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.rectangle("fill", 25, 25, max(#lvlName+12, #tilesetName+9)*7, 40)
+            love.graphics.setColor(255, 255, 255)
+            
+            love.graphics.print("Level name: " .. lvlName, 30, 30)
+            love.graphics.print("Tileset: " .. tilesetName, 30, 45)
+        end
+    elseif state == "inp" then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", 25, 25, (#inpVar + #inpStr + 3)*6+10, 25)
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.print(inpVar .. ": " .. inpStr .. "_", 30, 30)
+    end
 end
